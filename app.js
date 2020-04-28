f45b210c735ea589268973f27bc374a0fb211043
edwintian <rxchen87@gmail.com>
2020-04-28 16:21:58 +0800

    adding a new user route for authentication via jwt web token

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-
+const cookieParser = require("cookie-parser");
 // parse req.body as a json object
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies);
+  console.log('Signed Cookies: ', req.signedCookies);  
+  next();
+})
+
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
 const songsRouter = require("./routes/songs");
 app.use("/songs", songsRouter);
--- utils/helper.js
+++ utils/helper.js
@@ -1,3 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const getJWTSecret = () => {
+  const secret = process.env.JWT_SECRET_KEY;
+  if (!secret) {
+    throw new Error("Missing secrets to sign JWT token");
+  }
+  return secret;
+};
+
+const createJWTToken = username => {
+  const today = new Date();
+  const exp = new Date(today);
+
+  const secret = getJWTSecret();
+  exp.setDate(today.getDate() + 60); // adding days
+
+  const payload = { name: username, exp: parseInt(exp.getTime() / 1000) };
+  const token = jwt.sign(payload, secret);
+  return token;
+};
+
 const requireJsonContent = (req, res, next) => {
   if (req.headers["content-type"] !== "application/json") {
     res.status(400).send("Server wants application/json!");
@@ -6,4 +28,21 @@ const requireJsonContent = (req, res, next) => {
   }
 };
 
-module.exports = { requireJsonContent };
+const protectRoute = (req, res, next) => {
+  try {
+    if (!req.cookies.token) {
+      throw new Error("You are not authorized");
+    }
+    req.user = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
+    next();
+  } catch (err) {
+    err.statusCode = 401;
+    next(err);
+  }
+};
+
+module.exports = {
+  requireJsonContent,
+  createJWTToken,
+  protectRoute
+};
